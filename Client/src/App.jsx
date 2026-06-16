936a51a1adee076d51b3b6e04537d570f38d502f
Zaid Rangrez <zaidrangrez.me@gmail.com>
2026-06-16 16:15:57 +0530

    Login integretion done

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Login from './pages/Login.jsx'
+import {Routes , Route} from 'react-router-dom'
+import Notes from './pages/Notes.jsx'
+import Register from './pages/Register.jsx'
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+      <Route path='/login' element = {<Login/>} />
+      <Route path='/' element = {<Notes/>} />
+      <Route path='/register' element = {<Register/>} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
--- Server/src/app.js
+++ Server/src/app.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import morgan from 'morgan'
 import authRoutes from './routes/auth.routes.js'
+import cors from 'cors'
 import noteRoutes from './routes/note.routes.js'
 import cookieParser from 'cookie-parser'
 
 const app = express()
+app.use(cors({
+    origin : 'http://localhost:5173',
+    credentials : true,
+}))
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cookieParser())
