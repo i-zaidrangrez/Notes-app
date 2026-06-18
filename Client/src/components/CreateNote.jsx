d55fb923046fea727ce2cf6caa6524150b9bace4
Zaid Rangrez <zaidrangrez.me@gmail.com>
2026-06-18 17:04:33 +0530

    Create Note Panel Done

---
+++ Client/src/components/CreateNote.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const CreateNote = (props) => {
+
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        const Note = {
+            id : Date.now(),
+            title,
+            content
+        }
+        props.addNotes(Note)
+        
+        setTitle("")
+        setContent("")
+    }
+
+  return ( 
+    <div className=' h-screen  p-5 flex place-content-center flex-col gap-5'>
+        <h1 className='p-4 text-3xl font-bold '>Create New Note</h1>
+        <form className=' flex flex-col w-full gap-4 p-4 h-1/2 items-center' onSubmit={(e)=>{handleSubmit(e)}}>
+            <input value={title} type="text" placeholder='Add Title' onChange={(e)=>{
+                setTitle(e.target.value)
+            }} className='w-full p-3 outline-0 border rounded border-gray-400  ' />
+            <textarea value={content} onChange={(e)=>{
+                setContent(e.target.value)
+            }} placeholder='More Details' className='w-full h-3/4 p-4 outline-0 border rounded border-gray-400 '></textarea>
+            <button className='bg-green-300 text-black w-1/2 p-2 rounded'>Create Note</button>
+        </form>
+        
+    </div>
+  )
+}
+
+export default CreateNote
--- Client/src/components/Welcome.jsx
+++ Client/src/components/Welcome.jsx
@@ -2,15 +2,21 @@ import { Plus } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import React from 'react'
 
-const Welcome = () => {
+const Welcome = (props) => {
   return (
     <div className='flex place-content-between w-full p-2 flex-wrap '>
         <div className=''>
-            <h1 className='text-2xl text-blue-500 font-bold'>Welcome Back , John</h1>
+            <h1 className='text-2xl text-green-300 font-bold'>Welcome Back , John</h1>
             <p className='mt-[5%] text-gray-400'>Here Are Your Notes...</p>
         </div>
         <div>
-            <button className=' text-end flex items-center justify-center p-3 bg-blue-600 text-white font-mono rounded-md mt-[3%]'><Plus color="white" />Create Note</button>
+            <button onClick={()=>{
+              if(!props.createNote){
+                props.setCreateNote(true);
+              }else{
+                props.setCreateNote(false)
+              }
+             }} className=' text-end flex items-center justify-center p-3 bg-green-300 text-black font-mono rounded-md mt-[3%]'><Plus color="black" />{props.createNote?"Create Note" : "Cancle"}</button>
         </div>
     </div>
   )
